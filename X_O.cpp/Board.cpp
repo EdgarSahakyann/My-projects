dd6f91f8d9d125c6f9815068e4a29b40cd9a9b23
EdgarSahakyann <edgar061204@gmail.com>
2025-04-26 13:51:32 +0400

    Update Board.cpp

--- X_O.cpp/Board.cpp
+++ X_O.cpp/Board.cpp
@@ -3,22 +3,22 @@
 
 Board::Board(int s) {
 	size = s;
-	dasht = vector<vector<char>>(size, vector<char>(size, ' '));
+	_board = vector<vector<char>>(size, vector<char>(size, ' '));
 }
 
 void Board::initBoard() {
-	for (int i = 0; i < dasht.size(); ++i) {
-		for (int j = 0; j < dasht.size(); ++j) {
+	for (int i = 0; i < _board.size(); ++i) {
+		for (int j = 0; j < _board.size(); ++j) {
 
 			if (i == 1 || i == 3) {
-				dasht[i][j] = '-';
+				_board[i][j] = '-';
 			}
 			else {
 				if (j == 1 || j == 3) {
-					dasht[i][j] = '|';
+					_board[i][j] = '|';
 				}
 				else {
-					dasht[i][j] = ' ';
+					_board[i][j] = ' ';
 				}
 			}
 
@@ -29,24 +29,24 @@ void Board::initBoard() {
 void Board::showBoard(Position cursor) {
 	cout << "\033[32m" << "|-----|" << "\033[0m" << endl;
 
-	for (int i = 0; i < dasht.size(); ++i) {
-		for (int j = 0; j < dasht.size(); ++j) {
+	for (int i = 0; i < _board.size(); ++i) {
+		for (int j = 0; j < _board.size(); ++j) {
 
 			if (i == 0 && j == 0) {
 				cout << "\033[32m" << "|" << "\033[0m";
 			}
 
-			if (dasht[i][j] == 'X') {
-				cout << "\033[31m" << dasht[i][j] << "\033[0m";
+			if (_board[i][j] == 'X') {
+				cout << "\033[31m" << _board[i][j] << "\033[0m";
 			}
-			else if (dasht[i][j] == 'O') {
-				cout << "\033[34m" << dasht[i][j] << "\033[0m";
+			else if (_board[i][j] == 'O') {
+				cout << "\033[34m" << _board[i][j] << "\033[0m";
 			}
 			else if (i == cursor.row && j == cursor.col) {
 				cout << "\033[32m" << "*" << "\033[0m";
 			}
 			else {
-				cout << dasht[i][j];
+				cout << _board[i][j];
 			}
 
 		}
