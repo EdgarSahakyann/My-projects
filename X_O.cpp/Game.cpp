8503e803724d3ac3323d9ff8f7df14d81dd7787d
EdgarSahakyann <edgar061204@gmail.com>
2025-04-26 14:14:54 +0400

    Update Game.cpp

--- X_O.cpp/Game.cpp
+++ X_O.cpp/Game.cpp
@@ -1,7 +1,6 @@
 #include <ctime>
 #include "Game.h"
 
-
 Game::Game(string player1, string player2) : cursor(2, 2) {
 	name1 = player1;
 	name2 = player2;
@@ -12,10 +11,10 @@ void Game::move(Position to, int& cnt) {
 	if (validMove(to)) {
 		cnt++;
 		if (currentPlayer == 'X') {
-			dasht[to.row][to.col] = 'X';
+			_board[to.row][to.col] = 'X';
 		}
 		else {
-			dasht[to.row][to.col] = 'O';
+			_board[to.row][to.col] = 'O';
 		}
 	}
 }
@@ -34,28 +33,28 @@ void Game::switchPlayers() {
 }
 
 bool Game::XWon() {
-	if ((dasht[0][0] == 'X' && dasht[0][2] == 'X' && dasht[0][4] == 'X') ||
-		(dasht[2][0] == 'X' && dasht[2][2] == 'X' && dasht[2][4] == 'X') ||
-		(dasht[4][0] == 'X' && dasht[4][2] == 'X' && dasht[4][4] == 'X') ||
-		(dasht[0][0] == 'X' && dasht[2][0] == 'X' && dasht[4][0] == 'X') ||
-		(dasht[0][2] == 'X' && dasht[2][2] == 'X' && dasht[4][2] == 'X') ||
-		(dasht[0][4] == 'X' && dasht[2][4] == 'X' && dasht[4][4] == 'X') ||
-		(dasht[0][0] == 'X' && dasht[2][2] == 'X' && dasht[4][4] == 'X') ||
-		(dasht[0][4] == 'X' && dasht[2][2] == 'X' && dasht[4][0] == 'X')) {
+	if ((_board[0][0] == 'X' && _board[0][2] == 'X' && _board[0][4] == 'X') ||
+		(_board[2][0] == 'X' && _board[2][2] == 'X' && _board[2][4] == 'X') ||
+		(_board[4][0] == 'X' && _board[4][2] == 'X' && _board[4][4] == 'X') ||
+		(_board[0][0] == 'X' && _board[2][0] == 'X' && _board[4][0] == 'X') ||
+		(_board[0][2] == 'X' && _board[2][2] == 'X' && _board[4][2] == 'X') ||
+		(_board[0][4] == 'X' && _board[2][4] == 'X' && _board[4][4] == 'X') ||
+		(_board[0][0] == 'X' && _board[2][2] == 'X' && _board[4][4] == 'X') ||
+		(_board[0][4] == 'X' && _board[2][2] == 'X' && _board[4][0] == 'X')) {
 		return true;
 	}
 	return false;
 }
 
 bool Game::OWon() {
-	if ((dasht[0][0] == 'O' && dasht[0][2] == 'O' && dasht[0][4] == 'O') ||
-		(dasht[2][0] == 'O' && dasht[2][2] == 'O' && dasht[2][4] == 'O') ||
-		(dasht[4][0] == 'O' && dasht[4][2] == 'O' && dasht[4][4] == 'O') ||
-		(dasht[0][0] == 'O' && dasht[2][0] == 'O' && dasht[4][0] == 'O') ||
-		(dasht[0][2] == 'O' && dasht[2][2] == 'O' && dasht[4][2] == 'O') ||
-		(dasht[0][4] == 'O' && dasht[2][4] == 'O' && dasht[4][4] == 'O') ||
-		(dasht[0][0] == 'O' && dasht[2][2] == 'O' && dasht[4][4] == 'O') ||
-		(dasht[0][4] == 'O' && dasht[2][2] == 'O' && dasht[4][0] == 'O')) {
+	if ((_board[0][0] == 'O' && _board[0][2] == 'O' && _board[0][4] == 'O') ||
+		(_board[2][0] == 'O' && _board[2][2] == 'O' && _board[2][4] == 'O') ||
+		(_board[4][0] == 'O' && _board[4][2] == 'O' && _board[4][4] == 'O') ||
+		(_board[0][0] == 'O' && _board[2][0] == 'O' && _board[4][0] == 'O') ||
+		(_board[0][2] == 'O' && _board[2][2] == 'O' && _board[4][2] == 'O') ||
+		(_board[0][4] == 'O' && _board[2][4] == 'O' && _board[4][4] == 'O') ||
+		(_board[0][0] == 'O' && _board[2][2] == 'O' && _board[4][4] == 'O') ||
+		(_board[0][4] == 'O' && _board[2][2] == 'O' && _board[4][0] == 'O')) {
 		return true;
 	}
 	return false;
@@ -69,7 +68,7 @@ bool Game::gameOver() {
 }
 
 bool Game::validMove(Position to) {
-	if (dasht[to.row][to.col] != ' ') {
+	if (_board[to.row][to.col] != ' ') {
 		return false;
 	}
 	return true;
@@ -104,34 +103,34 @@ void Game::block() {
 	for (int i = 0; i < size; i += 2) {
 		for (int j = 0; j < size; j += 2) {
 
-			if (dasht[i][0] == 'X' && dasht[i][4] == 'X' && validMove(Position(i, 2))) {
-				dasht[i][2] = 'O';
+			if (_board[i][0] == 'X' && _board[i][4] == 'X' && validMove(Position(i, 2))) {
+				_board[i][2] = 'O';
 				alreadyMoved = true;
 				return;
 			}
-			else if (dasht[i][0] == 'X' && dasht[i][2] == 'X' && validMove(Position(i, 4))) {
-				dasht[i][4] = 'O';
+			else if (_board[i][0] == 'X' && _board[i][2] == 'X' && validMove(Position(i, 4))) {
+				_board[i][4] = 'O';
 				alreadyMoved = true;
 				return;
 			}
-			else if (dasht[i][2] == 'X' && dasht[i][4] == 'X' && validMove(Position(i, 0))) {
-				dasht[i][0] = 'O';
+			else if (_board[i][2] == 'X' && _board[i][4] == 'X' && validMove(Position(i, 0))) {
+				_board[i][0] = 'O';
 				alreadyMoved = true;
 				return;
 			}
 
-			else if (dasht[0][j] == 'X' && dasht[4][j] == 'X' && validMove(Position(2, j))) {
-				dasht[2][j] = 'O';
+			else if (_board[0][j] == 'X' && _board[4][j] == 'X' && validMove(Position(2, j))) {
+				_board[2][j] = 'O';
 				alreadyMoved = true;
 				return;
 			}
-			else if (dasht[0][j] == 'X' && dasht[2][j] == 'X' && validMove(Position(4, j))) {
-				dasht[4][j] = 'O';
+			else if (_board[0][j] == 'X' && _board[2][j] == 'X' && validMove(Position(4, j))) {
+				_board[4][j] = 'O';
 				alreadyMoved = true;
 				return;
 			}
-			else if (dasht[2][j] == 'X' && dasht[4][j] == 'X' && validMove(Position(0, j))) {
-				dasht[0][j] = 'O';
+			else if (_board[2][j] == 'X' && _board[4][j] == 'X' && validMove(Position(0, j))) {
+				_board[0][j] = 'O';
 				alreadyMoved = true;
 				return;
 			}
@@ -139,33 +138,33 @@ void Game::block() {
 		}
 	}
 
-	if (dasht[0][0] == 'X' && dasht[4][4] == 'X' && validMove(Position(2, 2))) {
-		dasht[2][2] = 'O';
+	if (_board[0][0] == 'X' && _board[4][4] == 'X' && validMove(Position(2, 2))) {
+		_board[2][2] = 'O';
 		alreadyMoved = true;
 		return;
 	}
-	else if (dasht[0][4] == 'X' && dasht[4][0] == 'X' && validMove(Position(2, 2))) {
-		dasht[2][2] = 'O';
+	else if (_board[0][4] == 'X' && _board[4][0] == 'X' && validMove(Position(2, 2))) {
+		_board[2][2] = 'O';
 		alreadyMoved = true;
 		return;
 	}
-	else if (dasht[0][0] == 'X' && dasht[2][2] == 'X' && validMove(Position(4, 4))) {
-		dasht[4][4] = 'O';
+	else if (_board[0][0] == 'X' && _board[2][2] == 'X' && validMove(Position(4, 4))) {
+		_board[4][4] = 'O';
 		alreadyMoved = true;
 		return;
 	}
-	else if (dasht[2][2] == 'X' && dasht[4][4] == 'X' && validMove(Position(0, 0))) {
-		dasht[0][0] = 'O';
+	else if (_board[2][2] == 'X' && _board[4][4] == 'X' && validMove(Position(0, 0))) {
+		_board[0][0] = 'O';
 		alreadyMoved = true;
 		return;
 	}
-	else if (dasht[0][4] == 'X' && dasht[2][2] == 'X' && validMove(Position(4, 0))) {
-		dasht[4][0] = 'O';
+	else if (_board[0][4] == 'X' && _board[2][2] == 'X' && validMove(Position(4, 0))) {
+		_board[4][0] = 'O';
 		alreadyMoved = true;
 		return;
 	}
-	else if (dasht[4][0] == 'X' && dasht[2][2] == 'X' && validMove(Position(0, 4))) {
-		dasht[0][4] = 'O';
+	else if (_board[4][0] == 'X' && _board[2][2] == 'X' && validMove(Position(0, 4))) {
+		_board[0][4] = 'O';
 		alreadyMoved = true;
 		return;
 	}
@@ -179,34 +178,34 @@ void Game::win() {
 	for (int i = 0; i < size; i += 2) {
 		for (int j = 0; j < size; j += 2) {
 
-			if (dasht[i][0] == 'O' && dasht[i][4] == 'O' && validMove(Position(i, 2))) {
-				dasht[i][2] = 'O';
+			if (_board[i][0] == 'O' && _board[i][4] == 'O' && validMove(Position(i, 2))) {
+				_board[i][2] = 'O';
 				alreadyWon = true;
 				return;
 			}
-			else if (dasht[i][0] == 'O' && dasht[i][2] == 'O' && validMove(Position(i, 4))) {
-				dasht[i][4] = 'O';
+			else if (_board[i][0] == 'O' && _board[i][2] == 'O' && validMove(Position(i, 4))) {
+				_board[i][4] = 'O';
 				alreadyWon = true;
 				return;
 			}
-			else if (dasht[i][2] == 'O' && dasht[i][4] == 'O' && validMove(Position(i, 0))) {
-				dasht[i][0] = 'O';
+			else if (_board[i][2] == 'O' && _board[i][4] == 'O' && validMove(Position(i, 0))) {
+				_board[i][0] = 'O';
 				alreadyWon = true;
 				return;
 			}
 
-			else if (dasht[0][j] == 'O' && dasht[4][j] == 'O' && validMove(Position(2, j))) {
-				dasht[2][j] = 'O';
+			else if (_board[0][j] == 'O' && _board[4][j] == 'O' && validMove(Position(2, j))) {
+				_board[2][j] = 'O';
 				alreadyWon = true;
 				return;
 			}
-			else if (dasht[0][j] == 'O' && dasht[2][j] == 'O' && validMove(Position(4, j))) {
-				dasht[4][j] = 'O';
+			else if (_board[0][j] == 'O' && _board[2][j] == 'O' && validMove(Position(4, j))) {
+				_board[4][j] = 'O';
 				alreadyWon = true;
 				return;
 			}
-			else if (dasht[2][j] == 'O' && dasht[4][j] == 'O' && validMove(Position(0, j))) {
-				dasht[0][j] = 'O';
+			else if (_board[2][j] == 'O' && _board[4][j] == 'O' && validMove(Position(0, j))) {
+				_board[0][j] = 'O';
 				alreadyWon = true;
 				return;
 			}
@@ -214,33 +213,33 @@ void Game::win() {
 		}
 	}
 
-	if (dasht[0][0] == 'O' && dasht[4][4] == 'O' && validMove(Position(2, 2))) {
-		dasht[2][2] = 'O';
+	if (_board[0][0] == 'O' && _board[4][4] == 'O' && validMove(Position(2, 2))) {
+		_board[2][2] = 'O';
 		alreadyWon = true;
 		return;
 	}
-	else if (dasht[0][4] == 'O' && dasht[4][0] == 'O' && validMove(Position(2, 2))) {
-		dasht[2][2] = 'O';
+	else if (_board[0][4] == 'O' && _board[4][0] == 'O' && validMove(Position(2, 2))) {
+		_board[2][2] = 'O';
 		alreadyWon = true;
 		return;
 	}
-	else if (dasht[0][0] == 'O' && dasht[2][2] == 'O' && validMove(Position(4, 4))) {
-		dasht[4][4] = 'O';
+	else if (_board[0][0] == 'O' && _board[2][2] == 'O' && validMove(Position(4, 4))) {
+		_board[4][4] = 'O';
 		alreadyWon = true;
 		return;
 	}
-	else if (dasht[2][2] == 'O' && dasht[4][4] == 'O' && validMove(Position(0, 0))) {
-		dasht[0][0] = 'O';
+	else if (_board[2][2] == 'O' && _board[4][4] == 'O' && validMove(Position(0, 0))) {
+		_board[0][0] = 'O';
 		alreadyWon = true;
 		return;
 	}
-	else if (dasht[0][4] == 'O' && dasht[2][2] == 'O' && validMove(Position(4, 0))) {
-		dasht[4][0] = 'O';
+	else if (_board[0][4] == 'O' && _board[2][2] == 'O' && validMove(Position(4, 0))) {
+		_board[4][0] = 'O';
 		alreadyWon = true;
 		return;
 	}
-	else if (dasht[4][0] == 'O' && dasht[2][2] == 'O' && validMove(Position(0, 4))) {
-		dasht[0][4] = 'O';
+	else if (_board[4][0] == 'O' && _board[2][2] == 'O' && validMove(Position(0, 4))) {
+		_board[0][4] = 'O';
 		alreadyWon = true;
 		return;
 	}
@@ -268,14 +267,14 @@ void Game::compMove(string difficulty, int& cnt) {
 	if (difficulty == "easy") {
 		if (!validMoves.empty()) {
 			finalMove = validMoves[rand() % validMoves.size()];
-			dasht[finalMove.row][finalMove.col] = 'O';
+			_board[finalMove.row][finalMove.col] = 'O';
 		}
 	}
 	else if (difficulty == "medium") {
 		block();
 		if (!validMoves.empty() && !alreadyMoved) {
 			finalMove = validMoves[rand() % validMoves.size()];
-			dasht[finalMove.row][finalMove.col] = 'O';
+			_board[finalMove.row][finalMove.col] = 'O';
 		}
 	}
 	else if (difficulty == "hard") {
@@ -284,8 +283,8 @@ void Game::compMove(string difficulty, int& cnt) {
 			block();
 		}
 		if (!validMoves.empty() && !alreadyMoved && !alreadyWon) {
-			finalMove = validMoves[rand() % validMoves.size()]; 
-			dasht[finalMove.row][finalMove.col] = 'O';
+			finalMove = validMoves[rand() % validMoves.size()];
+			_board[finalMove.row][finalMove.col] = 'O';
 		}
 	}
 
